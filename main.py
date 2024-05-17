48c37fb91234d96925b2c78a13f8b344829be8cb
mekhi-woods <mekhidw@hawaii.edu>
2024-05-16 14:38:44 -1000

    Adding SNpy data, main.py produces lightcurves for 91bg like Krisciunas SNe.

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+# import snpy
+
+FILTER_WHEEL = ['u', 'g', 'r', 'i', 'B']
+
 if __name__ == '__main__':
-    print()
+    KrisciunasPath = "targetLists\91bglike_justnames.txt"
+    KrisciunasNames = np.genfromtxt(KrisciunasPath, dtype=str, delimiter=', ')
+
+    allCPSPhot = "data\CSPdata\SN_photo.dat"
+    allCPSPhotData = np.genfromtxt(allCPSPhot, dtype='str')
+
+    names = allCPSPhotData[:,0]
+    filters = allCPSPhotData[:,1]
+    time = allCPSPhotData[:,2]
+    light = allCPSPhotData[:,3]
+    err = allCPSPhotData[:,4]
+
+    sigma = 5
+    for tar in KrisciunasNames:
+        for n in range(len(FILTER_WHEEL)):
+            # output_names = names[(names == tar) & (filters == FILTER_WHEEL[n])]
+            output_light = light[(names == tar) & (filters == FILTER_WHEEL[n])].astype('float64')
+            output_time = time[(names == tar) & (filters == FILTER_WHEEL[n])].astype('float64') + 53000
+            output_err = err[(names == tar) & (filters == FILTER_WHEEL[n])].astype('float64')
+            plt.errorbar(output_time, output_light, yerr=output_err*sigma, fmt='o', label=FILTER_WHEEL[n])
+
+        plt.title(tar); plt.xlabel('Time [MJD]'); plt.ylabel('Intensity [mag]')
+        plt.gca().invert_yaxis()
+        plt.legend()
+        plt.savefig('save\\'+str(tar)+'.png')
+        plt.show()
+
